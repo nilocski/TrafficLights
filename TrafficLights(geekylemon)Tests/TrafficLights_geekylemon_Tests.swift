11d1c883576687301d58392797505da961b78f90
Colin Mackenzie <nilocski@yahoo.com>
2016-03-29 13:11:39 +0200

    Initial Commit

---
+++ TrafficLights(geekylemon)Tests/TrafficLights_geekylemon_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  TrafficLights_geekylemon_Tests.swift
+//  TrafficLights(geekylemon)Tests
+//
+//  Created by Colin Mackenzie on 29/03/2016.
+//  Copyright © 2016 nilocski. All rights reserved.
+//
+
+import XCTest
+@testable import TrafficLights_geekylemon_
+
+class TrafficLights_geekylemon_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
